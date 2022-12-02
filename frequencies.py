92ae9eeb286f804697f2de2b0880eb8e54d04465
Alexander Grebenyuk <alexander.grebenyuk@kcl.ac.uk>
2022-12-02 23:14:37 +0000

    test

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,18 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        found = False
+        for storedItem in frequencies:
+            if(item!=storedItem):
+                pass
+            else:
+                found = True
+                frequencies['item'] = frequencies['item'] + 1
+                break
+            
+        if(found == False):
+            frequencies['item'] = 0
+
+
     return frequencies
