c7e50cec09bc6bc6af21e34a6e534e87a03d626e
Alexander Grebenyuk <alexander.grebenyuk@kcl.ac.uk>
2022-12-03 00:09:44 +0000

    test

--- frequencies.py
+++ frequencies.py
@@ -5,8 +5,8 @@ def frequencies(items):
     frequencies = {}
     # Your code goes here
     for item in items:
-        if item in frequencies:
+        if str(item) in frequencies:
             frequencies[item] = frequencies[item] + 1
         else:
-            frequencies[item] = 1
+            frequencies[str(item)] = 1
     return frequencies
